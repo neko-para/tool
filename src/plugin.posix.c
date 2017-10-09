42bdc2864cbb9388c8e3b65f43a8aa6392f872d8
neko-para <liao.hy@outlook.com>
2017-10-09 08:41:09 +0800

    Add ability to add prefix _ to in windows

--- src/plugin.posix.c
+++ src/plugin.posix.c
@@ -1,11 +1,22 @@
 #include "callf.h"
 #include <dlfcn.h>
+#include <stdlib.h>
+#include <string.h>
 
 void* loadplugin(const char* name) {
 	return dlopen(name, RTLD_LAZY);
 }
 void* loadfunction(void* handle, const char* name) {
+#ifdef _CALLF_EXTRA_PREFIX_
+	char* str = (char*)malloc(strlen(name) + 1);
+	void* ret;
+	strcpy(str, name);
+	ret = dlsym(handle, str);
+	free(str);
+	return ret;
+#else
 	return dlsym(handle, name);
+#endif
 }
 void freeplugin(void* handle) {
 	dlclose(handle);
--- src/plugin.win32.c
+++ src/plugin.win32.c
@@ -1,11 +1,22 @@
 #include <callf.h>
 #include <windows.h>
+#include <stdlib.>
+#include <string.h>
 
 void* loadplugin(const char* name) {
 	return (void*)LoadLibraryA(name);
 }
 void* loadfunction(void* handle, const char* name) {
+#ifdef _CALLF_EXTRA_PREFIX_
+	char* str = (char*)malloc(strlen(name) + 1);
+	void* ret;
+	strcpy(str, name);
+	ret = (void*)GetProcAddress((HINSTANCE)handle, str);
+	free(str);
+	return ret;
+#else
 	return (void*)GetProcAddress((HINSTANCE)handle, name);
+#endif
 }
 void freeplugin(void* handle) {
 	FreeLibrary((HINSTANCE)handle);
